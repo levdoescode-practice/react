5b280c969f002dc1becc8f9ca570bda50ea9bf24
LevDoesCode <levdoescode@gmail.com>
2024-08-21 13:43:56 -0500

    Added navigation bar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/home-page'
 import RecipePage from './pages/recipe-page'
+import NavBar from './components/ui/nav-bar'
 
 function App() {
   return (
     <>
+    <NavBar />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/recipes/:recipeId" element={<RecipePage />} />
