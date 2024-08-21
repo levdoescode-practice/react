a84a0bc07eb7231db66b623384365c696925c0a3
LevDoesCode <levdoescode@gmail.com>
2024-08-21 15:08:16 -0500

    Added Badge component using a map

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,9 +1,29 @@
-import { Button } from '@/components/ui/button';
+import { Badge } from "@/components/ui/badge";
 
 export default function HomePage() {
+    const cuisines: Array<string> = [
+        "All",
+        "Asian",
+        "American",
+        "Greek",
+        "Italian",
+        "Indian",
+        "Japanese",
+        "Mediterranean",
+        "Mexican",
+        "Pakistani",
+    ];
     return (
         <>
-            <Button>Hello. This is the Home Page</Button>
+            {cuisines.map((cuisine, idx) => (
+                <Badge
+                    key={`${cuisine}-${idx}`}
+                    variant={"outline"}
+                    className="border-orange-800 text-gray-900 text-lg mx-2 my-1 hover:cursor-pointer bg-orange-50 hover:scale-110 ease-in duration-200"
+                >
+                    {cuisine}
+                </Badge>
+            ))}
         </>
     );
 }
